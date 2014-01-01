39e185f0fc50d274fd4e6ab8a3f36e3ec799999f
Kelly Savietta <ksavietta@gmail.com>
2014-01-01 17:51:48 -0500

    added password matching integration test. everything is green!

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -29,6 +29,15 @@ feature 'sign up', %Q{
     expect(page).to have_content("can't be blank")
     expect(page).to_not have_content("Sign Out")
   end
-  scenario 'password confirmation does not match confirmation'
+  scenario 'password confirmation does not match confirmation' do
+    visit root_path
+    click_link 'Sign Up'
+    fill_in 'user_password', with: 'password'
+    fill_in 'Password Confirmation', with: 'notthesame'
+
+    click_button 'Sign Up'
+    expect(page).to have_content("doesn't match")
+    expect(page).to_not have_content("Sign Out")
+  end
 
 end
