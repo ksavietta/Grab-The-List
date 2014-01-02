ae2d89df4d55ad9e5d9f9fc47d48b3812b415fc8
Kelly Savietta <ksavietta@gmail.com>
2014-01-02 11:13:24 -0500

    added unhappy path integration tests for user sign in, all passing! user who is already signed in, can't sign in again. user who wants to sign in but doesn't have the right password get's a notice and cannot sign in.

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -5,21 +5,56 @@ feature 'user signs in', %Q{
   So that I can see my lists
   } do
 
-scenario ' an existing user specifies a valid email and password' do
-  user = FactoryGirl.create(:user)
-  visit root_path
-  click_link 'Sign In'
-  fill_in 'Email', with: user.email
-  fill_in 'Password', with: user.password
-  click_button 'Sign In'
+  scenario ' an existing user specifies a valid email and password' do
+    user = FactoryGirl.create(:user)
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign In'
 
-  expect(page).to have_content('Welcome Back!')
-  expect(page).to have_content('Sign Out')
-end
+    expect(page).to have_content('Welcome Back!')
+    expect(page).to have_content('Sign Out')
+  end
 
-scenario 'a nonexistant email and password is supplied'
-scenario 'a exisiting email with the wrong password is denied access'
-scenario 'an already authenticated user cannot re-sign in'
+  scenario 'a nonexistant email and password is supplied' do
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'Email', with: 'notreal@example.com'
+    fill_in 'Password', with: 'notarealpassword'
+    click_button 'Sign In'
 
+    expect(page).to have_content('Invalid email or password')
+    expect(page).to_not have_content('Welcome Back!')
+    expect(page).to_not have_content('Sign Out')
+
+  end
+
+  scenario 'a exisiting email with the wrong password is denied access' do
+    user = FactoryGirl.create(:user)
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'nottherightpassword'
+    click_button 'Sign In'
+
+    expect(page).to have_content('Invalid email or password')
+    expect(page).to_not have_content('Welcome Back!')
+    expect(page).to_not have_content('Sign Out')
+  end
+
+  scenario 'an already authenticated user cannot re-sign in' do
+    user = FactoryGirl.create(:user)
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign In'
+
+    expect(page).to have_content('Sign Out')
+    expect(page).to_not have_content('Sign In')
+
+    visit new_user_session_path
+    expect(page).to have_content('You are already signed in.')
+  end
 
 end
