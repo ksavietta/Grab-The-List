0d566330815792cb928539dab08f4d49d0640f9a
Kelly Savietta <ksavietta@gmail.com>
2014-01-03 11:04:49 -0500

    added user creates list integration test

--- spec/features/user_creates_list_spec.rb
+++ spec/features/user_creates_list_spec.rb
@@ -1,3 +1,33 @@
 require 'spec_helper'
 
-feature 'create list'
+feature 'create list', %Q{As a user,
+I would like to create a list and optionally add items to it
+so that I can add lists to my account.} do
+
+  scenario 'user visits new list creation page' do
+    visit root_path
+    click_button 'New List'
+
+    expect(page).to have_content('New List')
+  end
+
+  scenario 'user creates new list' do
+    visit new_list_path
+    fill_in 'Name', with: 'Test'
+    fill_in 'Description', with: 'Test Description'
+    fill_in 'Price', with: '12.00'
+    click_button 'Create List'
+
+    expect(page).to have_content('List was successfully created')
+  end
+
+  scenario 'user tries to create list without filling in field' do
+    visit new_list_path
+
+    click_button 'Create List'
+
+    expect(page).to have_content("can't be blank")
+    expect(page).to_not have_content('List was successfully created')
+  end
+
+end
