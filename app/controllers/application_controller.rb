247387665e21f93c2c780854c899833cfc4e6ab6
Kelly Savietta <ksavietta@gmail.com>
2014-01-02 19:01:46 -0500

    added sign out feature from devise, needs additional testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name]
   end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 end
