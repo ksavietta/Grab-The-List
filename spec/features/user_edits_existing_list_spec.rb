df95a5e79c54af83ba212ffc5c268b2cd4275434
Kelly Savietta <ksavietta@gmail.com>
2014-01-03 12:17:55 -0500

    made modificatons to the list controller and and list views, in order to get the user-edits-a-list integration test to pass. Have yet to integrate the items partial into the list views. all green!

---
+++ spec/features/user_edits_existing_list_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+feature 'edit list', %Q{As a user,
+I would like to edit a list
+so that I can see updated lists on my account.} do
+
+  scenario 'user visits list page and selects list' do
+    FactoryGirl.create(:list)
+    visit root_path
+    click_link 'ListName1'
+    expect(page).to have_content('ListName1')
+
+    click_link 'ListName1'
+    expect(page).to have_content("Make Changes to")
+    fill_in 'Name', with: 'TestTest'
+    fill_in 'Description', with: 'Test Description'
+    fill_in 'Price', with: '1'
+
+    click_button 'Update List'
+
+    expect(page).to have_content("List was successfully updated")
+  end
+
+end
