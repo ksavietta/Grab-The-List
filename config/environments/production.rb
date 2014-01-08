f63575907afcf9363e92d3e8e01286010fce65dd
Kelly Savietta <ksavietta@gmail.com>
2014-01-07 21:30:16 -0500

    added action mailer to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,13 @@ GtlSecond::Application.configure do
 
   config.action_mailer.default_url_options ={:host =>'http://grab-the-list-staging.herokuapp.com/'}
   config.action_mailer.delivery_method =:smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['heroku.com']
+  }
 end
