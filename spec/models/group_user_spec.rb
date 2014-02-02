d763a5a66731686cc9546388b250d711a9ab0794
Kelly Savietta <ksavietta@gmail.com>
2014-02-02 15:24:40 -0500

    added membership new member test

--- spec/models/group_user_spec.rb
+++ spec/models/group_user_spec.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 describe GroupUser do
   it { should validate_presence_of :user}
 
@@ -5,5 +7,22 @@ describe GroupUser do
   it { should belong_to(:user)}
 
   it {should have_many(:purchases)}
+
+
+  it 'should query an existing user by email using virtual attribute setter' do
+    user2 = FactoryGirl.create(:user)
+    membership = FactoryGirl.create(:group_user)
+
+    membership.email_address=(user2.email)
+    expect(membership.group.users.count).to_not eq(2)
+  end
+
+  it 'should not set a non-existant email to a membership' do
+    user2 = FactoryGirl.create(:user)
+    membership = FactoryGirl.create(:group_user)
+
+    membership.email_address=("notarealaddress@example.com")
+    expect(membership.group.users.count).to eq(1)
+  end
 
 end
